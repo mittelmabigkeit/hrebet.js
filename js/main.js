0514f52709669791d779005ef38aa306b2c73fcc
mittelmabigkeit <comp422012016@mail.ru>
2016-11-02 18:01:30 +0300

    ne ochen udachno

--- js/main.js
+++ js/main.js
@@ -11,16 +11,35 @@ var Person = Backbone.Model.extend({
     }
 });
 
+var person = new Person();
+
 var PeopleCollection = Backbone.Collection.extend({
     model:Person
 });
 
+var PeopleView = Backbone.View.extend({
+    tagName: 'ul',
+
+    initialize: function() {
+        console.log(this.collection);
+    },
+
+    render: function() {
+        this.collection.each(function(person) {
+            //console.log(person);
+            var personView=new PersonView({model:person});
+            //console.log(personView);
+            this.$el.append(personView.render().el);
+
+        }, this);
+    }
+});
 
 var PersonView = Backbone.View.extend({
     tagName: 'li',
-
-    template: _.template($('#person-id').html()),
-    mytemplate: _.template($('#myperson-id').html()),
+    template: _.template('<strong>Меня зовут <%=name%>, мне <%=age%> лет, моя профессия <%=job%> и я говорю - <%=words%> !</strong>'),
+    //template: _.template($('#person-id').html()),
+   // mytemplate: _.template($('#myperson-id').html()),
 
     //template: '#person-id',
     //mytemplate: '#myperson-id',
@@ -30,10 +49,10 @@ var PersonView = Backbone.View.extend({
     },
 
     render: function() {
-       this.$el.html(this.template(this.model.toJSON()) + this.mytemplate(this.model.toJSON()));
+       //this.$el.html(this.template(this.model.toJSON()) + this.mytemplate(this.model.toJSON()));
        // var template = _.template( $(this.template).html());
        // var mytemplate = _.template( $(this.mytemplate).html());
-        //this.$el.html(template(this.model.toJSON()));
+        this.$el.html(this.template(this.model.toJSON()));
         //this.$el.html(template(this.model.toJSON()) + mytemplate(this.model.toJSON()));
         //this.$el.html(this.mytemplate(this.model.toJSON()));
         $(document.body).append(this.el);
@@ -57,14 +76,16 @@ var peopleCollection = new PeopleCollection([
     }
 ]);
 
-var person = new Person;
-var personView = new PersonView({model: person});
+var peopleView = new PeopleView({collection: peopleCollection});
 
-var person2 = new Person({'name': 'Semen', 'age': '666'});
-var personView2 = new PersonView({model: person2});
-
-peopleCollection.add(person);
-peopleCollection.add(person2);
+//var person = new Person;
+//var personView = new PersonView({model: person});
+//
+//var person2 = new Person({'name': 'Semen', 'age': '666'});
+//var personView2 = new PersonView({model: person2});
+//
+//peopleCollection.add(person);
+//peopleCollection.add(person2);
 
 //CONSOLE COMMANDS
 //personView;
