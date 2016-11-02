5b2131d08680aa190ea64ca386d8df59abdba7d4
mittelmabigkeit <comp422012016@mail.ru>
2016-11-02 17:43:29 +0300

    array

--- js/main.js
+++ js/main.js
@@ -11,35 +11,60 @@ var Person = Backbone.Model.extend({
     }
 });
 
+var PeopleCollection = Backbone.Collection.extend({
+    model:Person
+});
+
+
 var PersonView = Backbone.View.extend({
     tagName: 'li',
 
-    //template: _.template($('#person-id').html()),
-    //mytemplate: _.template($('#myperson-id').html()),
+    template: _.template($('#person-id').html()),
+    mytemplate: _.template($('#myperson-id').html()),
 
-    template: '#person-id',
-    mytemplate: '#myperson-id',
+    //template: '#person-id',
+    //mytemplate: '#myperson-id',
 
     initialize: function() {
         this.render();
     },
 
     render: function() {
-       // this.$el.html(this.template(this.model.toJSON()) + this.mytemplate(this.model.toJSON()));
-        var template = _.template( $(this.template).html());
-        var mytemplate = _.template( $(this.mytemplate).html());
+       this.$el.html(this.template(this.model.toJSON()) + this.mytemplate(this.model.toJSON()));
+       // var template = _.template( $(this.template).html());
+       // var mytemplate = _.template( $(this.mytemplate).html());
         //this.$el.html(template(this.model.toJSON()));
-        this.$el.html(template(this.model.toJSON()) + mytemplate(this.model.toJSON()));
+        //this.$el.html(template(this.model.toJSON()) + mytemplate(this.model.toJSON()));
         //this.$el.html(this.mytemplate(this.model.toJSON()));
         $(document.body).append(this.el);
     }
 });
+
+var peopleCollection = new PeopleCollection([
+    {
+        name: 'Ivan',
+        age: 23,
+        job: 'Taxi driver'
+    },
+    {
+        name: 'Anna',
+        age: 20,
+        job: 'student'
+    },
+    {
+        name: 'Pavel',
+        job: 'Builder'
+    }
+]);
 
 var person = new Person;
 var personView = new PersonView({model: person});
 
 var person2 = new Person({'name': 'Semen', 'age': '666'});
 var personView2 = new PersonView({model: person2});
+
+peopleCollection.add(person);
+peopleCollection.add(person2);
 
 //CONSOLE COMMANDS
 //personView;
